ed5e1cbbcc1de5a15393415a2e9578a0f6542f4f
dave <dave@T480-DAVE.test>
2020-10-22 17:46:28 +0100

    simple schema and validation

--- src/main/java/net/cornforth/VehicleRoute.java
+++ src/main/java/net/cornforth/VehicleRoute.java
@@ -1,6 +1,8 @@
 package net.cornforth;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.support.processor.validation.SchemaValidationException;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,6 +18,17 @@ public class VehicleRoute extends RouteBuilder {
 
         from("direct:add")
             .id("add-vehicle")
-            .log("${body}");
+            .doTry()
+                .convertBodyTo(java.lang.String.class, "UTF-8")
+                .log("add")
+                .to("validator:addVehicle.xsd")
+            .doCatch(SchemaValidationException.class)
+                .setHeader(Exchange.HTTP_RESPONSE_CODE, simple("400"))
+                .log("${exception.message}")
+            .doCatch(Exception.class)
+                .setHeader(Exchange.HTTP_RESPONSE_CODE, simple("500"))
+                .log("${exception.message}")
+            .end();
+        ;
     }
 }
